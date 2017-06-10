37bbc163158c9cd85f291ec5dda138056ea818e7
Benjamin Babik <work@benjaminbabik.com>
2017-06-10 16:16:23 +0100

    fixing names

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-ECHO is on.
+const _ = require('lodash')
+
+let gruad = require('./gruad.json')
+let yougov = require('./yougov.json')
+
+_.range(yougov.length).forEach(
+  i => {
+
+    let needle = yougov[i].constituency
+    let result = gruad.filter(r => r.name == needle)
+    if (result.length == 0) {
+      console.log("could not find " + needle)
+    }
+  }
+)
